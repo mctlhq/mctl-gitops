1ddcb5073808d01c6dc1c1065ed9461236528b0b
mashkovd <mashkovd@live.ru>
2026-02-14 23:56:59 +0100

    delete backstage.tf

--- infrastructure/k3s-preview/cluster-bootstrap/backstage.tf
+++
@@ -1,3 +0,0 @@
-# All Backstage secrets managed by Vault + ExternalSecrets:
-# - backstage-secrets: platform-gitops/apps/templates/backstage-secrets.yaml
-# - ghcr-credentials: platform-gitops/apps/templates/backstage-ghcr.yaml
